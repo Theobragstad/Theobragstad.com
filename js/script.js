48d29491724cce43ae05ccafb4cf2bca1e4623b2
Theo Bragstad <theo.bragstad@colorado.edu>
2023-03-19 23:17:30 -0600

    style

--- js/script.js
+++ js/script.js
@@ -5,20 +5,22 @@ function setSrc(element, src) {
 
 
 
-var bgColorIndex = Math.floor(Math.random() * 2);
-var colorIndex = Math.floor(Math.random() * 2);
-var names = document.getElementsByClassName("name");
-var bgColor = "#ffc008";
-var color = "#fdfd96";
+// var bgColorIndex = Math.floor(Math.random() * 2);
+// var colorIndex = Math.floor(Math.random() * 2);
+// var names = document.getElementsByClassName("name");
+// var bgColor = "#ffc008";
+// var color = "#fdfd96";
 
-if(bgColorIndex == 0) {
-    bgColor = "#bdf6d9";
-}
-if(colorIndex == 0) {
-    color = "#1e90ff";
-}
+// if(bgColorIndex == 0) {
+//     bgColor = "#bdf6d9";
+// }
+// if(colorIndex == 0) {
+//     color = "#1e90ff";
+// }
 
-for(let i = 0; i < names.length; i++) {
-    names[i].style.backgroundColor = bgColor;
-    names[i].style.color = color;
-}
+// for(let i = 0; i < names.length; i++) {
+//     names[i].style.backgroundColor = bgColor;
+//     names[i].style.color = color;
+// }
+
+
